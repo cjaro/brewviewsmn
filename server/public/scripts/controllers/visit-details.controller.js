c4e7a70e86a9b787d16ae3132c13a9e86dbfd58d
Catherine J <cjarocki@gmail.com>
2017-09-05 18:56:14 -0500

    commit quiero cervezas

--- server/public/scripts/controllers/visit-details.controller.js
+++ server/public/scripts/controllers/visit-details.controller.js
@@ -1,73 +1,79 @@
 app.controller('VisitDetailsController', ['$http', '$location', 'DataFactory', '$routeParams',
-function($http, $location, DataFactory, $routeParams) {
-  console.log('visit details controller loaded');
-  // console.log('routeParams', $routeParams)
-  var self = this;
-  self.testVariable = 'I am a test!';
-  self.visitList = [];
-  self.visit = DataFactory.visit;
-  DataFactory.getVisitDetails($routeParams.visitID);
-
-  // console.log('self.visit', self.visit);
-
-  self.addNewBrew = function(newBrewObject) {
-    newBrewObject.visit_id = $routeParams.visitID;
-    console.log('newBrewObject: ', newBrewObject);
-    $http.post('/visitDetails', newBrewObject).then(function(response) {
-      self.newBeer = {};
-      DataFactory.getVisitDetails($routeParams.visitID);
-    });
-  };
-
-  self.deleteBrew = function(brewID) {
-    console.log('brewID to delete: ', brewID);
-    swal({
-      title: 'Are you sure?',
-      text: "You can't undo this!",
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#185FEF',
-      cancelButtonColor: '#FF1300',
-      confirmButtonText: 'Delete'
-    }).then(function() {
-      $http.delete('/visitDetails/' + brewID)
-      swal(
-        'Deleted!',
-        'This brew has been deleted :(',
-        'success'
-      ).then(function(response) {})
-      DataFactory.getVisitDetails($routeParams.visitID);
-    });
-  };
-
-  self.updateBrew = function(brew) {
-    console.log('brew: ', brew);
-    swal("Brew updated!")
-    $http.put('/visitDetails/' + brew.id, brew).then(function(response) {
-      DataFactory.getVisitDetails($routeParams.visitID);
-    });
-  };
-
-  self.deleteVisit = function(visitID) {
-    console.log('visit to delete: ', visitID);
-    swal({
-      title: 'Are you sure?',
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#185FEF',
-      cancelButtonColor: '#FF1300',
-      confirmButtonText: 'Delete Brew'
-    }).then(function() {
-      $http.delete('/visits/' + visitID)
-      swal(
-        'This visit has been deleted!',
-        'success'
-      ).then(function(response) {
+    function($http, $location, DataFactory, $routeParams) {
+        console.log('visit details controller loaded');
+        // console.log('routeParams', $routeParams)
+        var self = this;
+        self.testVariable = 'I am a test!';
+        self.visitList = [];
+        self.visit = DataFactory.visit;
         DataFactory.getVisitDetails($routeParams.visitID);
-        // $location.url('/main');
-      })
-    });
-  }
-}
+
+        // console.log('self.visit', self.visit);
+
+        self.addNewBrew = function(newBrewObject) {
+            newBrewObject.visit_id = $routeParams.visitID;
+            console.log('newBrewObject: ', newBrewObject);
+            $http.post('/visitDetails', newBrewObject).then(function(response) {
+                self.newBeer = {};
+                DataFactory.getVisitDetails($routeParams.visitID);
+                //want to add option to star or mark brews added to hall of fame
+                //  ie:
+                // if (brewIsRated>8) {
+                //  add star/mark to brew name on visit-details page
+                // } else {
+                // don't do the thing};
+            });
+        };
+
+        self.deleteBrew = function(brewID) {
+            console.log('brewID to delete: ', brewID);
+            swal({
+                title: 'Are you sure?',
+                text: "You can't undo this!",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#185FEF',
+                cancelButtonColor: '#FF1300',
+                confirmButtonText: 'Delete'
+            }).then(function() {
+                $http.delete('/visitDetails/' + visitID + brewID)
+                swal(
+                    'Deleted!',
+                    'This brew has been deleted :(',
+                    'success'
+                ).then(function(response) {})
+                DataFactory.getVisitDetails($routeParams.visitID);
+            });
+        };
+
+        self.updateBrew = function(brew) {
+            console.log('brew: ', brew);
+            swal("Brew updated!")
+            $http.put('/visitDetails/' + brew.id, brew).then(function(response) {
+                DataFactory.getVisitDetails($routeParams.visitID);
+            });
+        };
+
+        self.deleteVisit = function(visitID) {
+            console.log('visit to delete: ', visitID);
+            swal({
+                title: 'Are you sure?',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#185FEF',
+                cancelButtonColor: '#FF1300',
+                confirmButtonText: 'Delete Brew'
+            }).then(function() {
+                $http.delete('/visits/' + visitID)
+                swal(
+                    'This visit has been deleted!',
+                    'success'
+                ).then(function(response) {
+                    DataFactory.getVisitDetails($routeParams.visitID);
+                    // $location.url('/main');
+                })
+            });
+        }
+    }
 
 ]);
--- server/public/scripts/factories/auth.factory.js
+++ server/public/scripts/factories/auth.factory.js
@@ -1,48 +1,49 @@
 app.factory('AuthFactory', ['FirebaseAuthFactory', '$http', '$location', '$window', function(FirebaseAuthFactory, $http, $location, $window) {
-  var auth = FirebaseAuthFactory;
-  var loggedIn = {};
-  var userInfo = { info: '' };
-  var newLoggedIn = { loginStatus: false };
+    var auth = FirebaseAuthFactory;
+    var loggedIn = {};
+    var userInfo = { info: '' };
+    var newLoggedIn = { loginStatus: false };
 
-  auth.$onAuthStateChanged(function(firebaseUser) {
-    // Check directly if firebaseUser is null
-    newLoggedIn.loginStatus = !!firebaseUser;
-    loggedIn.value = firebaseUser !== null;
-    if (loggedIn.value) {
-      getUserData(firebaseUser);
-      $location.path('/main');
-    } else {
-      console.log('user is not logged in');
-    }
-  });
-
-  function logIn() {
-    console.log('login clicked');
-    auth.$signInWithPopup("google").then(function(firebaseUser) {
-      console.log("Firebase Authenticated: ", firebaseUser.user.displayName);
-    }).catch(function(error) {
-      console.log("Authentication failed: ", error);
-    });
-  }
-
-  function getUserData(firebaseUser) {
-    firebaseUser.getToken().then(function(idToken){
-      $http({
-        method: 'GET',
-        url: '/users',
-        headers: {
-          id_token: idToken
+    auth.$onAuthStateChanged(function(firebaseUser) {
+        // Check directly if firebaseUser is null
+        newLoggedIn.loginStatus = !!firebaseUser;
+        loggedIn.value = firebaseUser !== null;
+        if (loggedIn.value) {
+            getUserData(firebaseUser);
+            $location.path('/main');
+        } else {
+            console.log('user is not logged in');
+            $location.path('/login');
         }
-      }).then(function(response) {
-        console.log('this garbage can function getUserData is not working blargh fuck');
-        userInfo.info = response.data;
-      });
     });
-  }
 
-  return {
-    logIn: logIn,
-    userInfo: userInfo,
-    newLoggedIn: newLoggedIn
-  };
+    function logIn() {
+        console.log('login clicked');
+        auth.$signInWithPopup("google").then(function(firebaseUser) {
+            console.log("Firebase Authenticated: ", firebaseUser.user.displayName);
+        }).catch(function(error) {
+            console.log("Authentication failed: ", error);
+        });
+    }
+
+    function getUserData(firebaseUser) {
+        firebaseUser.getToken().then(function(idToken) {
+            $http({
+                method: 'GET',
+                url: '/users',
+                headers: {
+                    id_token: idToken
+                }
+            }).then(function(response) {
+                console.log('this garbage can function getUserData is not working blargh fuck');
+                userInfo.info = response.data;
+            });
+        });
+    }
+
+    return {
+        logIn: logIn,
+        userInfo: userInfo,
+        newLoggedIn: newLoggedIn
+    };
 }]);
