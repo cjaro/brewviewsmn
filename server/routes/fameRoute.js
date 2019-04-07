4877cf736c26e8147d99e68765ed5cbf9b591467
Catherine J <cjarocki@gmail.com>
2019-04-07 00:04:38 -0500

    massive updates, but had to reset so am working from memory to replicate stashed, deleted changes

--- server/routes/fameRoute.js
+++ server/routes/fameRoute.js
@@ -2,21 +2,25 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../modules/pg-pool.js');
 
-router.get('/', function(req, res){
-  // var fameBrews = req.query.fameBrews
-  // console.log('getting hall of fame beers: ', fameBrews);
-  pool.connect()
-      .then(function(client) {
-          client.query('SELECT brews_test.beer_name, brews_test.rating, brews_test.notes, visits.brewery, visits.date_had FROM brews_test JOIN visits ON brews_test.visit_id = visits.id WHERE rating >= 9 ORDER BY date_had DESC;')
-              .then(function(result) {
-                  client.release();
-                  res.send(result.rows);
-              })
-              .catch(function(err) {
-                  console.log('error on SELECT', err);
-                  res.sendStatus(500);
-              });
-      });
+router.get('/', function(req, res) {
+    // var fameBrews = req.query.fameBrews console.log('getting hall of fame beers:
+    // ', fameBrews);
+    pool
+        .connect()
+        .then(function(client) {
+            client
+                .query('SELECT brews_test.beer_name, brews_test.rating, brews_test.notes, visits.brewery' +
+                    ', visits.date_had FROM brews_test JOIN visits ON brews_test.visit_id = visits.id' +
+                    ' WHERE rating >= 9 ORDER BY rating DESC;')
+                .then(function(result) {
+                    client.release();
+                    res.send(result.rows);
+                })
+                .catch(function(err) {
+                    console.log('error on SELECT', err);
+                    res.sendStatus(500);
+                });
+        });
 });
 
 module.exports = router;
