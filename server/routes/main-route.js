76ad16e35b2fe04198524b5af5fd36c68658de6b
Catherine J <cjarocki@gmail.com>
2019-09-15 14:17:19 -0500

    small feature of adding map link and visit location field

--- server/routes/main-route.js
+++ server/routes/main-route.js
@@ -6,56 +6,56 @@ var admin = require('firebase-admin');
 var serviceAccount = require('../firebase-service-account.json');
 
 router.get('/', function(req, res) {
-  pool.connect()
-  .then(function(client) {
-    client.query('SELECT * FROM visits ORDER BY date_had DESC;')
-    .then(function(result) {
-      client.release();
-      res.send(result.rows);
-    })
-    .catch(function(err) {
-      console.log('error on SELECT', err);
-      res.sendStatus(500);
-    });
-  });
+    pool.connect()
+        .then(function(client) {
+            client.query('SELECT * FROM visits ORDER BY date_had DESC;')
+                .then(function(result) {
+                    client.release();
+                    res.send(result.rows);
+                })
+                .catch(function(err) {
+                    console.log('error on SELECT', err);
+                    res.sendStatus(500);
+                });
+        });
 });
 
 router.post('/', function(req, res) {
-  var newVisit = req.body;
-  console.log('New visit: ', newVisit);
-  pool.connect()
-  .then(function(client) {
-    client.query('INSERT INTO visits (date_had, brewery, b_notes) VALUES ($1, $2, $3) RETURNING id', [newVisit.date_had, newVisit.brewery, newVisit.b_notes])
-    .then(function(result) {
-      client.release();
-      res.send(result.rows);
-    })
-    .catch(function(err) {
-      console.log('error on INSERT', err);
-      res.sendStatus(500);
-    });
-  });
+    var newVisit = req.body;
+    console.log('New visit: ', newVisit);
+    pool.connect()
+        .then(function(client) {
+            client.query('INSERT INTO visits (date_had, brewery, b_notes, location) VALUES ($1, $2, $3) RETURNING id', [newVisit.date_had, newVisit.brewery, newVisit.b_notes, newVisit.location])
+                .then(function(result) {
+                    client.release();
+                    res.send(result.rows);
+                })
+                .catch(function(err) {
+                    console.log('error on INSERT', err);
+                    res.sendStatus(500);
+                });
+        });
 });
 
 router.delete('/:id', function(req, res) {
-  var brewRecordDeleteID = req.body;
-  console.log('brewRecordDeleteID = req.params.id', req.body)
-  console.log('Deleting brewID main: ', brewRecordDeleteID);
-  pool.connect()
-  .then(function(client) {
-    client.query('DELETE FROM brews_test WHERE visit_id=$1;', [brewRecordDeleteID])
-    .then(function(result){
-      client.query('DELETE FROM visits WHERE id=$1;')
-    })
-    .then(function(result) {
-      client.release();
-      res.sendStatus(200);
-    })
-    .catch(function(err) {
-      console.log('error on SELECT', err);
-      res.sendStatus(500);
-    });
-  });
+    var brewRecordDeleteID = req.body;
+    console.log('brewRecordDeleteID = req.params.id', req.body)
+    console.log('Deleting brewID main: ', brewRecordDeleteID);
+    pool.connect()
+        .then(function(client) {
+            client.query('DELETE FROM brews_test WHERE visit_id=$1;', [brewRecordDeleteID])
+                .then(function(result) {
+                    client.query('DELETE FROM visits WHERE id=$1;')
+                })
+                .then(function(result) {
+                    client.release();
+                    res.sendStatus(200);
+                })
+                .catch(function(err) {
+                    console.log('error on SELECT', err);
+                    res.sendStatus(500);
+                });
+        });
 });
 
 module.exports = router;
